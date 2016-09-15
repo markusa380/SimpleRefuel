dedfb5ccf458f9636556c2bcb6564fcbe44fb28c
MarkusA380 <markusappel@hotmail.de>
2016-09-15 10:39:41 +0200

    Applauncher utilization added

--- Source/SimpleRefuel.cs
+++ Source/SimpleRefuel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KSP.UI.Screens;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,30 +8,29 @@ namespace SimpleRefuel
     [KSPAddon(KSPAddon.Startup.Flight, false)]
     public class Refuel : MonoBehaviour
     {
-        public bool canRefuel = false;
+        /* GUI FIELDS */
+        private int guid;
+        private Rect WindowRect;
+        private bool visible = true;
+        private GUIStyle style = GUIStyle.none;
+        private GUIStyle style2 = new GUIStyle(Instantiate(HighLogic.Skin).button);
 
-        int guid;
-        Rect WindowRect;
-        bool visible = true;
-        GUIStyle style = GUIStyle.none;
-        GUIStyle style2 = new GUIStyle(Instantiate(HighLogic.Skin).button);
-        bool refuelling = false;
-        Vessel vessel;
-        int i;
-        int o;
-        bool select_fuel = false;
+        /* PRIVATE FIELDS */
+        private bool refuelling = false;
+        private Vessel vessel;
+        private int i;
+        private int o;
+        private bool select_fuel = false;
+        private List<string> resources = new List<string>();
+        private int current_resource = 0;
+        private bool canRefuel = false;
 
-        List<string> resources = new List<string>();
-        int current_resource = 0;
+        /* TOOLBAR FIELDS */
+        private ApplicationLauncherButton button;
+        private readonly string icon_green = "SimpleRefuel/Textures/icon_green";
+        private readonly string icon_red = "SimpleRefuel/Textures/icon_red";
 
-        void Reset()
-        {
-            refuelling = false;
-            i = 0;
-            o = 0;
-            select_fuel = false;
-        }
-
+        /* MONOBEHAVIOUR METHODS */
         void Awake()
         {
             GameEvents.onShowUI.Add(OnShowUI);
@@ -39,8 +39,9 @@ namespace SimpleRefuel
             guid = Guid.NewGuid().GetHashCode();
 
             WindowRect = new Rect(Screen.width / 2f - 100f, Screen.height * 0.6f, 200f, 10f);
-        }
 
+            CreateStockToolbar();
+        }
         void Update()
         {
             if(FlightGlobals.ActiveVessel != vessel)
@@ -49,13 +50,15 @@ namespace SimpleRefuel
                 vessel = FlightGlobals.ActiveVessel;
             }
 
-            if( (int)vessel.srfSpeed == 0 && (vessel.situation == Vessel.Situations.LANDED || vessel.situation == Vessel.Situations.PRELAUNCH) && (vessel.transform.position - SpaceCenter.Instance.transform.position).sqrMagnitude <= SpaceCenter.Instance.AreaRadius * SpaceCenter.Instance.AreaRadius  )
+            if ((int)vessel.srfSpeed == 0 && (vessel.situation == Vessel.Situations.LANDED || vessel.situation == Vessel.Situations.PRELAUNCH) && (vessel.transform.position - SpaceCenter.Instance.transform.position).sqrMagnitude <= SpaceCenter.Instance.AreaRadius * SpaceCenter.Instance.AreaRadius  )
             {
                 canRefuel = true;
+                ChangeButtonTexture(icon_green);
             }
             else
             {
                 canRefuel = false;
+                ChangeButtonTexture(icon_red);
                 Reset();
             }
 
@@ -104,73 +107,122 @@ namespace SimpleRefuel
                 }
             }
         }
+        void Destroy()
+        {
+            DeleteStockToolbar();
+        }
 
+        /* GUI METHODS */
         void OnGUI()
         {
-            if(canRefuel && visible && !refuelling)
+            if(canRefuel && select_fuel && visible && !refuelling)
             {
                 WindowRect = GUILayout.Window(guid, WindowRect, GUI, "", style);
             }
         }
-
         void GUI(int id)
         {
             GUILayout.BeginVertical();
-            if (!select_fuel)
-            {
-                GUILayout.BeginHorizontal();
-                GUILayout.Space(60);
-                if (GUILayout.Button("Refuel", style2, GUILayout.ExpandWidth(true)))
-                {
-                    select_fuel = true;
-                    resources.Clear();
-                    foreach(Part p in FlightGlobals.ActiveVessel.Parts)
-                    {
-                        foreach(PartResource r in p.Resources)
-                        {
-                            if(!resources.Contains(r.resourceName))
-                            {
-                                resources.Add(r.resourceName);
-                            }
-                        }
-                    }
-                }
-                GUILayout.Space(60);
-                GUILayout.EndHorizontal();
-            }
-            else
-            {
-                GUILayout.BeginHorizontal();
-                if(GUILayout.Button("<", style2, GUILayout.Width(20)))
-                {
-                    current_resource--;
-                    if (current_resource < 0)
-                        current_resource = resources.Count - 1;
-                }
-                if (GUILayout.Button(resources[current_resource], style2, GUILayout.Width(160)))
-                {
-                    refuelling = true;
-                }
-                if (GUILayout.Button(">", style2, GUILayout.Width(20)))
-                {
-                    current_resource++;
-                    if (current_resource > resources.Count - 1)
-                        current_resource = 0;
-                }
-                GUILayout.EndHorizontal();
 
+            GUILayout.BeginHorizontal();
+            if(GUILayout.Button("<", style2, GUILayout.Width(20)))
+            {
+                current_resource--;
+                if (current_resource < 0)
+                    current_resource = resources.Count - 1;
             }
+            if (GUILayout.Button(resources[current_resource], style2, GUILayout.Width(160)))
+            {
+                    refuelling = true;
+            }
+            if (GUILayout.Button(">", style2, GUILayout.Width(20)))
+            {
+                current_resource++;
+                if (current_resource > resources.Count - 1)
+                    current_resource = 0;
+            }
+            GUILayout.EndHorizontal();
+
             GUILayout.EndVertical();
         }
-
         void OnShowUI()
         {
             visible = true;
         }
-
         void OnHideUI()
         {
             visible = false;
+        }
+
+        /* PRIVATE METHODS */
+        private void Reset()
+        {
+            refuelling = false;
+            i = 0;
+            o = 0;
+            select_fuel = false;
+            resources.Clear();
+        }
+
+        /* TOOLBAR METHODS */
+        private void CreateStockToolbar()
+        {
+            if (!button)
+            {
+                button = ApplicationLauncher.Instance.AddModApplication(
+                () =>
+                {
+                    Open();
+                },
+                () =>
+                {
+                    Open();
+                },
+                null,
+                null,
+                null,
+                null,
+                ApplicationLauncher.AppScenes.FLIGHT,
+                GameDatabase.Instance.GetTexture(icon_red, false)
+                );
+            }
+        }
+        private void DeleteStockToolbar()
+        {
+            if (button)
+            {
+                ApplicationLauncher.Instance.RemoveModApplication(button);
+                button = null;
+            }
+        }
+        private void ChangeButtonTexture(string path)
+        {
+            button.SetTexture(GameDatabase.Instance.GetTexture(path, false));
+        }
+        private void Open()
+        {
+            resources.Clear();
+            foreach(Part p in vessel.Parts)
+            {
+                foreach(PartResource r in p.Resources)
+                {
+                    if(!resources.Contains(r.resourceName))
+                    {
+                        resources.Add(r.resourceName);
+                    }
+                }
+            }
+
+            if (canRefuel && !select_fuel)
+            {
+                if (resources.Count > 0)
+                    select_fuel = true;
+            }
+            else
+            {
+                select_fuel = false;
+                refuelling = false;
+            }
         }
     }
 }
